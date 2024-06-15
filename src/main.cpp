181217b6156a6827a2bac1b48bec798dd2b0576d
Sen Morgan <technars@gmail.com>
2024-06-15 16:48:32 +0200

    feat: add ESP-NOW example and platformio.ini override

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,86 @@
-#include <Arduino.h>
+#include <esp_now.h>
 #include <WiFi.h>
 
+#include <Wire.h>
 
-void setup() {
-  Serial.begin(9600);
+// REPLACE WITH THE MAC Address of your receiver
+uint8_t broadcastAddress[] = {CONTROLLER_MAC};
 
-  // Print MAC address
-  Serial.print("ESP Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message
+{
+    int var;
+} struct_message;
+
+// Create a struct_message to hold incoming data
+struct_message incomingReadings;
+
+esp_now_peer_info_t peerInfo;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+    Serial.print("\r\nLast Packet Send Status:\t");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-void loop() {
+// Callback when data is received
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
+    memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+    Serial.print("Bytes received: ");
+    Serial.println(len);
+    Serial.print("Int received: ");
+    Serial.println(incomingReadings.var);
+    Serial.println();
 
+    // Send echo
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&incomingReadings, sizeof(incomingReadings));
+    if (result == ESP_OK)
+    {
+        Serial.println("Sent with success");
+    }
+    else
+    {
+        Serial.println("Error sending the data");
+    }
+}
+
+void setup()
+{
+    // Init Serial Monitor
+    Serial.begin(115200);
+
+    // Set device as a Wi-Fi Station
+    WiFi.mode(WIFI_STA);
+
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK)
+    {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+
+    // Once ESPNow is successfully Init, we will register for Send CB to get the status of Transmitted packet
+    esp_now_register_send_cb(OnDataSent);
+
+    // Register peer
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+
+    // Add peer
+    if (esp_now_add_peer(&peerInfo) != ESP_OK)
+    {
+        Serial.println("Failed to add peer");
+        return;
+    }
+    // Register for a callback function that will be called when data is received
+    esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+}
+
+void loop()
+{
+    delay(100);
 }
