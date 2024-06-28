b0b824a4c35dc42a92843136f4337a0f2bd7e38b
Sen Morgan <technars@gmail.com>
2024-06-28 23:07:35 +0200

    refactor: use ESP-IDF GPIOs naming

--- include/constants.h
+++ include/constants.h
@@ -17,32 +17,32 @@
 #define LEVERS_COUNT 6
 
 // IO pins
-// #define RGB_LED_BUILTIN 48
+// #define RGB_LED_BUILTIN GPIO_NUM_48
 
 // Lights
-#define BOOM_LIGHTS 12
+#define BOOM_LIGHTS GPIO_NUM_12
 
 // Limit switches
 // #define BOOM_LOW_LIMIT
 // #define BOOM_HIGH_LIMIT
-#define BUCKET_ROLL_IN_LIMIT  14    // PWM at startup - change with boom lights?
-#define BUCKET_ROLL_OUT_LIMIT 13
-#define STICK_ROLL_IN_LIMIT   11
-#define STICK_ROLL_OUT_LIMIT  10
-#define SWING_CENTER_SWITCH   19
+#define BUCKET_ROLL_IN_LIMIT  GPIO_NUM_14    // PWM at startup - change with boom lights?
+#define BUCKET_ROLL_OUT_LIMIT GPIO_NUM_13
+#define STICK_ROLL_IN_LIMIT   GPIO_NUM_11
+#define STICK_ROLL_OUT_LIMIT  GPIO_NUM_10
+#define SWING_CENTER_SWITCH   GPIO_NUM_19
 
 // Motor pins
-#define BOOM_MOTOR_POS         35
-#define BOOM_MOTOR_NEG         36
-#define BUCKET_MOTOR_POS       39
-#define BUCKET_MOTOR_NEG       40
-#define STICK_MOTOR_POS        38
-#define STICK_MOTOR_NEG        37
-#define SWING_MOTOR_POS        47
-#define SWING_MOTOR_NEG        48
-#define LEFT_TRAVEL_MOTOR_POS  2
-#define LEFT_TRAVEL_MOTOR_NEG  1
-#define RIGHT_TRAVEL_MOTOR_POS 42
-#define RIGHT_TRAVEL_MOTOR_NEG 41
+#define BOOM_MOTOR_POS         GPIO_NUM_35
+#define BOOM_MOTOR_NEG         GPIO_NUM_36
+#define BUCKET_MOTOR_POS       GPIO_NUM_39
+#define BUCKET_MOTOR_NEG       GPIO_NUM_40
+#define STICK_MOTOR_POS        GPIO_NUM_38
+#define STICK_MOTOR_NEG        GPIO_NUM_37
+#define SWING_MOTOR_POS        GPIO_NUM_47
+#define SWING_MOTOR_NEG        GPIO_NUM_48
+#define LEFT_TRAVEL_MOTOR_POS  GPIO_NUM_2
+#define LEFT_TRAVEL_MOTOR_NEG  GPIO_NUM_1
+#define RIGHT_TRAVEL_MOTOR_POS GPIO_NUM_42
+#define RIGHT_TRAVEL_MOTOR_NEG GPIO_NUM_41
 
 #endif // _CONSTANTS_H
