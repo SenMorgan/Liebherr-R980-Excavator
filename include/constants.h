20d014e6d12c963e4ee72f713e9990bd7602809f
Sen Morgan <technars@gmail.com>
2024-06-30 21:15:40 +0200

    feat: control motors and lights via PCA9685

--- include/constants.h
+++ include/constants.h
@@ -25,27 +25,33 @@
 // Lights
 #define BOOM_LIGHTS GPIO_NUM_12
 
+// Lights connected to PCA9685
+#define LEFT_LIGHT         0
+#define RIGHT_LIGHT        1
+#define CABIN_FRONT_LIGHTS 2
+#define CABIN_BACK_LIGHTS  3
+
 // Limit switches
 // #define BOOM_LOW_LIMIT
 // #define BOOM_HIGH_LIMIT
-#define BUCKET_ROLL_IN_LIMIT  GPIO_NUM_14    // PWM at startup - change with boom lights?
+#define BUCKET_ROLL_IN_LIMIT  GPIO_NUM_14 // PWM at startup - change with boom lights?
 #define BUCKET_ROLL_OUT_LIMIT GPIO_NUM_13
 #define STICK_ROLL_IN_LIMIT   GPIO_NUM_11
 #define STICK_ROLL_OUT_LIMIT  GPIO_NUM_10
 #define SWING_CENTER_SWITCH   GPIO_NUM_19
 
-// Motor pins
-#define BOOM_MOTOR_POS         GPIO_NUM_35
-#define BOOM_MOTOR_NEG         GPIO_NUM_36
-#define BUCKET_MOTOR_POS       GPIO_NUM_39
-#define BUCKET_MOTOR_NEG       GPIO_NUM_40
-#define STICK_MOTOR_POS        GPIO_NUM_38
-#define STICK_MOTOR_NEG        GPIO_NUM_37
-#define SWING_MOTOR_POS        GPIO_NUM_47
-#define SWING_MOTOR_NEG        GPIO_NUM_48
-#define LEFT_TRAVEL_MOTOR_POS  GPIO_NUM_2
-#define LEFT_TRAVEL_MOTOR_NEG  GPIO_NUM_1
-#define RIGHT_TRAVEL_MOTOR_POS GPIO_NUM_42
-#define RIGHT_TRAVEL_MOTOR_NEG GPIO_NUM_41
+// Motor driver pins connected to PCA9685
+#define BOOM_MOTOR_POS         14
+#define BOOM_MOTOR_NEG         15
+#define BUCKET_MOTOR_POS       11
+#define BUCKET_MOTOR_NEG       10
+#define STICK_MOTOR_POS        9
+#define STICK_MOTOR_NEG        8
+#define SWING_MOTOR_POS        12
+#define SWING_MOTOR_NEG        13
+#define LEFT_TRAVEL_MOTOR_POS  6
+#define LEFT_TRAVEL_MOTOR_NEG  7
+#define RIGHT_TRAVEL_MOTOR_POS 5
+#define RIGHT_TRAVEL_MOTOR_NEG 4
 
 #endif // _CONSTANTS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,20 @@
 #include <WiFi.h>
-#include <Wire.h>
 #include "driver/temp_sensor.h"
-#include "GyverMotor2.h"
+#include <SPI.h>
+#include <Adafruit_PWMServoDriver.h>
 
 #include "constants.h"
 #include "data_structures.h"
 #include "esp_now_manager.h"
 #include "wifi_ota_manager.h"
 
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
 // Create a variable to store the received data
 controller_data_struct receivedData;
 
 // Create a variable to store the data that will be sent to the Controller
 excavator_data_struct dataToSend;
-
-// TODO: only 8 PWM pins available on ESP32
-GMotor2<DRIVER2WIRE_PWM> boomMotor(BOOM_MOTOR_POS, BOOM_MOTOR_NEG);
-// GMotor2<DRIVER2WIRE_PWM> bucketMotor(BUCKET_MOTOR_POS, BUCKET_MOTOR_NEG);
-// GMotor2<DRIVER2WIRE_PWM> stickMotor(STICK_MOTOR_POS, STICK_MOTOR_NEG);
-GMotor2<DRIVER2WIRE_PWM> swingMotor(SWING_MOTOR_POS, SWING_MOTOR_NEG);
-GMotor2<DRIVER2WIRE_PWM> leftTravelMotor(LEFT_TRAVEL_MOTOR_POS, LEFT_TRAVEL_MOTOR_NEG);
-GMotor2<DRIVER2WIRE_PWM> rightTravelMotor(RIGHT_TRAVEL_MOTOR_POS, RIGHT_TRAVEL_MOTOR_NEG);
 
 int16_t cpuTemp = -999;
 uint32_t lastTempReadTime = 0;
@@ -42,6 +36,39 @@ int16_t getCpuTemp()
     return (int16_t)(cpuTempF * 100.0);
 }
 
+void controlMotor(uint8_t posPin, uint8_t negPin, int16_t speed, bool breakMode = false)
+{
+    // Map -255 to 255 range to 0 to 4095 PWM range
+    int pwmValue = map(abs(speed), 0, 255, 0, 4095);
+
+    if (speed > 0)
+    {
+        // Forward direction
+        pwm.setPWM(posPin, 0, pwmValue);
+        pwm.setPWM(negPin, 0, 0);
+    }
+    else if (speed < 0)
+    {
+        // Reverse direction
+        pwm.setPWM(posPin, 0, 0);
+        pwm.setPWM(negPin, 0, pwmValue);
+    }
+    else
+    {
+        // If break mode is enabled, set both pins to HIGH
+        if (breakMode)
+        {
+            pwm.setPWM(posPin, 0, 4095);
+            pwm.setPWM(negPin, 0, 4095);
+        }
+        else
+        {
+            pwm.setPWM(posPin, 0, 0);
+            pwm.setPWM(negPin, 0, 0);
+        }
+    }
+}
+
 // Callback when data from Controller received
 void onDataFromController(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
@@ -52,18 +79,31 @@ void onDataFromController(const uint8_t *mac, const uint8_t *incomingData, int l
                   receivedData.leverPositions[3], receivedData.leverPositions[4], receivedData.leverPositions[5],
                   receivedData.buttonsStates[0], receivedData.buttonsStates[1], receivedData.battery);
 
-    boomMotor.setSpeed(receivedData.leverPositions[0]);
-    // bucketMotor.setSpeed(receivedData.leverPositions[1]);
-    // stickMotor.setSpeed(receivedData.leverPositions[2]);
-    swingMotor.setSpeed(receivedData.leverPositions[3]);
-    leftTravelMotor.setSpeed(receivedData.leverPositions[4]);
-    rightTravelMotor.setSpeed(receivedData.leverPositions[5]);
+    // Control motors based on received data
+    controlMotor(BOOM_MOTOR_POS, BOOM_MOTOR_NEG, receivedData.leverPositions[0], true);
+    controlMotor(BUCKET_MOTOR_POS, BUCKET_MOTOR_NEG, receivedData.leverPositions[1], true);
+    controlMotor(STICK_MOTOR_POS, STICK_MOTOR_NEG, receivedData.leverPositions[2], true);
+    controlMotor(SWING_MOTOR_POS, SWING_MOTOR_NEG, receivedData.leverPositions[3]);
+    controlMotor(LEFT_TRAVEL_MOTOR_POS, LEFT_TRAVEL_MOTOR_NEG, receivedData.leverPositions[4]);
+    controlMotor(RIGHT_TRAVEL_MOTOR_POS, RIGHT_TRAVEL_MOTOR_NEG, receivedData.leverPositions[5]);
 
-    // Send the dataToSend to the Controller
-    dataToSend.uptime = millis() / 1000;
-    dataToSend.battery = 100;
-    dataToSend.cpuTemp = cpuTemp;
-    sendDataToController(dataToSend);
+    // Control lights
+    if (receivedData.buttonsStates[0])
+    {
+        digitalWrite(BOOM_LIGHTS, HIGH);
+        pwm.setPWM(CABIN_FRONT_LIGHTS, 0, 4095);
+        pwm.setPWM(CABIN_BACK_LIGHTS, 0, 4095);
+        pwm.setPWM(LEFT_LIGHT, 0, 4095);
+        pwm.setPWM(RIGHT_LIGHT, 0, 4095);
+    }
+    else
+    {
+        digitalWrite(BOOM_LIGHTS, LOW);
+        pwm.setPWM(CABIN_FRONT_LIGHTS, 0, 0);
+        pwm.setPWM(CABIN_BACK_LIGHTS, 0, 0);
+        pwm.setPWM(LEFT_LIGHT, 0, 0);
+        pwm.setPWM(RIGHT_LIGHT, 0, 0);
+    }
 }
 
 void setup()
@@ -77,16 +117,15 @@ void setup()
     pinMode(STICK_ROLL_OUT_LIMIT, INPUT_PULLUP);
     pinMode(SWING_CENTER_SWITCH, INPUT_PULLUP);
 
-    pinMode(BOOM_MOTOR_POS, OUTPUT);
-    pinMode(BOOM_MOTOR_NEG, OUTPUT);
-
     // pinMode(RGB_LED_BUILTIN, OUTPUT);
     pinMode(BOOM_LIGHTS, OUTPUT);
 
     digitalWrite(BOOM_LIGHTS, HIGH);
 
-    leftTravelMotor.reverse(true);
-    rightTravelMotor.reverse(true);
+    // Init PWM with maximum frequency
+    pwm.begin();
+    pwm.setOscillatorFrequency(27000000);
+    pwm.setPWMFreq(1600);
 
     // Init Temp Sensor
     initTempSensor();
